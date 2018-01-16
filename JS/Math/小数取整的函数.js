2bfa525c7f4c5b4e7edb398b6fc4a011fc108c39
guojia <guojia@sunlands.com>
2018-01-16 10:54:45 +0800

    add demo file

---
+++ JS/Math/小数取整的函数.js
@@ -0,0 +1,14 @@
+
+
+// 1.丢弃小数部分,保留整数部分    3 
+console.log( parseInt(7/2) )   
+
+// 2.向上取整,有小数就整数部分加1     4
+console.log( Math.ceil(7/2) )
+
+// 3.四舍五入.     3
+// 只关心小数点后第一位
+console.log( Math.round(3.49) ) 
+
+// 4.向下取整   3
+console.log( Math.floor(7/2) )      

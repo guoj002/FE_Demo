313e4fea2ed07066438de0fe0c15349311873ae3
guojia <guojia@sunlands.com>
2018-07-10 16:49:54 +0800

    commot

--- git/header.js
+++ git/header.js
@@ -1,10 +1,12 @@
-测试文件二
 import React from 'react'
 import { Tooltip, Tabs } from 'antd'
 import PropTypes from 'prop-types'
 import { hashHistory } from 'react-router'
+import classnames from 'classnames'
+import { getJSON } from '@sunl-fe/dataservice'
 
 import Constants from 'constants/header'
+import URLS from '../../../constants/URLS'
 import style from './newMsgBox.less'
 import { MsgSquare } from '../../msgSys/Index'
 
@@ -18,6 +20,20 @@ class MsgBox extends React.Component {
             boxVisible: false,
             isLoading: false,
             activeKey: '1',
+            tabDataStructure: [
+                {
+                    tabKey: '1',
+                    hasUnRead: false,
+                    messageType: -2,
+                    tabName: '关注',
+                },
+                {
+                    tabKey: '2',
+                    hasUnRead: false,
+                    messageType: -1,
+                    tabName: '全部',
+                },
+            ],
         }
     }
 
@@ -28,8 +44,19 @@ class MsgBox extends React.Component {
         // console.log('visible', visible)
         this.setState({ boxVisible: visible, activeKey: '1' })
         if (visible) {
-            this.setState({ isLoading: true }, this.fetchMessagesBox(-1))
+            this.setState({ isLoading: true }, this.fetchMessagesBox(-2))
         }
+    }
+
+    getUnreadCounts = index => {
+        const { tabDataStructure } = this.state
+        getJSON(URLS.GET_UNREAD_COUNTS, messageType: tabDataStructure[index].messageType)
+            .then(data => {
+                this.setState({ tabDataStructure[index].hasUnRead: data.unreadMessageCount !== 0 })
+            })
+            .catch(c => {
+                alert(c)
+            })
     }
 
     fetchMessagesBox = (type = -1) => {
@@ -58,7 +85,7 @@ class MsgBox extends React.Component {
     }
 
     tabSelected = activeKey => {
-        const type = activeKey === 2 ? -1 : 1
+        const type = activeKey === '2' ? -1 : -2 // 全部 -1 、关注 -2
         this.setState({ activeKey, isLoading: true }, this.fetchMessagesBox(type))
     }
 
@@ -82,23 +109,40 @@ class MsgBox extends React.Component {
             wrappedIcon,
             activeWrappedIcon,
             unreadCounts = 0,
+            importForm,
+            // msgBoxStyle,
+            // msgWrapperIconStyle,
         } = this.props
         const unreadNum = unreadCounts * 1
         const {
             boxVisible,
             activeKey,
+            tabDataStructure,
         } = this.state
         const ttt = (
             <div id="new_msg_box" className={style['msg-box']}>
-                <Tabs defaultActiveKey="1" activeKey={activeKey} onChange={this.tabSelected}>
-                    <TabPane tab="关注" key="1">{this.renderTabPane()}</TabPane>
-                    <TabPane tab="全部" key="2" >{this.renderTabPane()}</TabPane>
+                <Tabs activeKey={activeKey} onChange={this.tabSelected}>
+                    {tabDataStructure.map((item, index) => (
+                        <TabPane
+                            tab={<div style={{ position: 'relative' }}>{item.tabName}{ this.getUnreadCounts(index) ? <span className={style['square-title-circle']} /> : null }</div>}
+                            key={item.tabKey}
+                        >
+                            {this.renderTabPane()}
+                        </TabPane>
+                    ))}
                 </Tabs>
             </div>
         )
         return (
-            <Tooltip visible={boxVisible} trigger="click" onVisibleChange={this.onVisibleChange} overlayClassName={style['msg-box-tooltip']} placement="bottomLeft" title={ttt}>
-                <div className={style['msg-wrapper-icon']}>
+            <Tooltip
+                visible={boxVisible}
+                trigger="click"
+                onVisibleChange={this.onVisibleChange}
+                overlayClassName={importForm === 'old' ? style['msg-box-tooltip--old'] : style['msg-box-tooltip']}
+                placement="bottomLeft"
+                title={ttt}
+            >
+                <div className={classnames({ [style['msg-wrapper-icon']]: true, [style['msg-wrapper-icon--old']]: importForm === 'old' })}>
                     { boxVisible ? activeWrappedIcon : wrappedIcon }
                     {unreadNum > 0 && <span className={style['msg-unread']}>{unreadNum <= 50 ? unreadNum : '50+'}</span>}
                 </div>
@@ -112,6 +156,9 @@ MsgBox.propTypes = {
     activeWrappedIcon: PropTypes.object,
     boxMsg: PropTypes.array,
     unreadCounts: PropTypes.number,
+    importForm: PropTypes.string,
+    // msgBoxStyle: PropTypes.object,
+    // msgWrapperIconStyle: PropTypes.object,
 }
 
 MsgBox.defaultProps = {
@@ -119,6 +166,9 @@ MsgBox.defaultProps = {
     activeWrappedIcon: null,
     boxMsg: [],
     unreadCounts: 0,
+    importForm: '',
+    // msgBoxStyle: {},
+    // msgWrapperIconStyle: {},
 }
 
 export default MsgBox

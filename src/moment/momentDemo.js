c2a39a93df54b429986a2c76fdcc46576c23b330
guojia <guojia@sunlands.com>
2018-08-06 17:51:31 +0800

    add 18-08-06

--- src/moment/momentDemo.js
+++ src/moment/momentDemo.js
@@ -1,2 +1,23 @@
 const moment = require('moment');
+
+// 1.当前时间
+const now = moment();
+
+// 2.字符串
+const day = moment("1995-12-25");
+
 console.log(moment())
+console.log(moment().format())
+
+console.log(now)
+console.log(now.format())
+
+console.log(day)
+console.log(day.format())
+
+// 3.字符串 + 格式
+// moment(String, String);
+// moment(String, String, String);
+// moment(String, String, Boolean);
+// moment(String, String, String, Boolean);
+
